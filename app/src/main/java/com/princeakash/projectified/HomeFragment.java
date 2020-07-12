758cb2a1988b02224d9514ced494249e6f9d2d97
TheAkash2002 <mahapatraakash@gmail.com>
2020-07-12 19:16:03 +0530

    Modified HomeFragment, frag_candidate_details_recruiter

--- app/src/main/java/com/princeakash/projectified/HomeFragment.java
+++ app/src/main/java/com/princeakash/projectified/HomeFragment.java
@@ -10,12 +10,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements HomeAdapter.HomeListener {
+
+    private ArrayList<HomeAdapter.HomeItem> list;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -63,7 +67,18 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.frag_home, container, false);
         RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
-        recyclerView.setAdapter(new HomeAdapter());
+
+        list = new ArrayList<>();
+        list.add(new HomeAdapter.HomeItem("App Development", R.mipmap.app_dev));
+        list.add(new HomeAdapter.HomeItem("Web Development", R.mipmap.web));
+        list.add(new HomeAdapter.HomeItem("Machine Learning", R.mipmap.ml));
+        list.add(new HomeAdapter.HomeItem("Artificial Intelligence", R.mipmap.artificial_intelligence));
+        list.add(new HomeAdapter.HomeItem("Content Writing", R.mipmap.content));
+        list.add(new HomeAdapter.HomeItem("UI/UX Design", R.mipmap.ui));
+        list.add(new HomeAdapter.HomeItem("Video Editing", R.mipmap.video));
+        list.add(new HomeAdapter.HomeItem("Others", R.mipmap.misc));
+
+        recyclerView.setAdapter(new HomeAdapter(list, getContext(), this));
         recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2, RecyclerView.VERTICAL, false));
         return view;
     }
--- app/src/main/java/com/princeakash/projectified/MainActivity.java
+++ app/src/main/java/com/princeakash/projectified/MainActivity.java
@@ -1,15 +1,27 @@
 package com.princeakash.projectified;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import static androidx.navigation.Navigation.findNavController;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView navigationView;
+    NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.frag_floatopportuninty);
+        setContentView(R.layout.activity_main);
 
+        navigationView = findViewById(R.id.bottomNavigation);
+        navController = findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 }
